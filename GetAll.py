5171cf19df7f714dfdc271d56be7d4c1d82980b6
wenchiehlee <wjlee@csie.nctu.edu.tw>
2025-07-08 12:14:08 +0800

    extend time out to make sure type 1, 4, 5 can download successful..

--- GetAll.py
+++ GetAll.py
@@ -96,7 +96,7 @@ def run_get_good_info(stock_id, parameter, debug_mode=False, direct_mode=False):
         env['PYTHONIOENCODING'] = 'utf-8'
         
         # Adjust timeout based on data type (special workflows need more time)
-        timeout = 90 if parameter in ['5', '7'] else 60  # Extra time for special workflows
+        timeout = 150 if parameter in ['5', '7'] else 120  # Extra time for special workflows
         
         # Run the command
         result = subprocess.run(cmd, 
